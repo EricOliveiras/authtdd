ad9767b28de74c79cd9ffd91579736c01621afe0
Eric Diego Oliveira do Nascimento <88443797+EricOliveiras@users.noreply.github.com>
2022-01-26 10:36:36 -0300

    modified config db and run migration

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
+  dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
   logging: false,
   define: {
     timestamps: true,
