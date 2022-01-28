8f14645450bb96a7f0c69e78375b46150eb946ee
Eric Diego Oliveira do Nascimento <88443797+EricOliveiras@users.noreply.github.com>
2022-01-28 10:05:54 -0300

    created controller and utils to tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,16 +1,32 @@
+const request = require('supertest');
+
 const User = require('../../src/database/models/User');
+const app = require('../../src/app');
+const truncate = require('../utils/truncate');
+
 require('../../src/database/')
 
 describe('Authentication', () => {
-  it('should create user', async () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should authentication with valid credential', async () => {
     const user = await User.create({
       name: 'Eric',
       email: 'eric@email.com',
       password_hash: '123456',
     });
-      
-    console.log(user);
 
-    expect(user.name).toBe('Eric'); 
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: 'eric@email.com',
+        password: '123451',
+      });
+
+
+    expect(response.status).toBe(200);
+
   });
 });
